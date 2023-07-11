37039fb33e5170ef6463460157d52473e3379363
bernardesvini <vberalves@gmail.com>
2023-07-11 21:50:48 +0100

    Enums Created

---
+++ RecipesSystem/Models/Enums/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace RecipesSystem.Models.Enums
+{
+    public enum Category
+    {
+        Italian = 0,
+        Asiatic = 1,
+        Mediterranean = 2,
+        Japanese = 3,
+        Brazilian = 4,
+        Arabian = 5,
+        Chinese = 6,
+        Portuguese = 7,
+        French = 8,
+        Georgian = 9
+    }
+}
---
+++ RecipesSystem/Models/Enums/Difficult.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipesSystem.Models.Enums
+{
+    public enum Difficult
+    {
+        Easy = 0,
+        Medium = 1,
+        Hard = 2,
+        Expert =3
+    }
+}
