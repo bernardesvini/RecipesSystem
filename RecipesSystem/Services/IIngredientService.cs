03e81f608fe98caf12759bb54499b579606e2a09
bernardesvini <vberalves@gmail.com>
2023-07-12 20:13:42 +0100

    Ingredient Service

---
+++ RecipesSystem/Services/IIngredientService.cs
@@ -0,0 +1,14 @@
+﻿using RecipesSystem.Models;
+using System.Collections.Generic;
+
+namespace RecipesSystem.Services
+{
+    public interface IIngredientService
+    {
+        List<Ingredient> RetrieveAll();
+        Ingredient Retrieve(int id);
+        void Create(Ingredient ingredient);
+        void Update(Ingredient ingredient);
+        void Delete(int id);
+    }
+}
---
+++ RecipesSystem/Services/IngredientService.cs
@@ -0,0 +1,35 @@
+﻿using RecipesSystem.Models;
+using RecipesSystem.Repository;
+using System.Collections.Generic;
+
+namespace RecipesSystem.Services
+{
+    public class IngredientService : IIngredientService
+    {
+        private readonly IIngredientRepository _ingredientRepository = new IngredientRepository();
+        public void Create(Ingredient ingredient)
+        {
+            _ingredientRepository.Create(ingredient);
+        }
+
+        public void Delete(int id)
+        {
+            _ingredientRepository.Delete(id);
+        }
+
+        public Ingredient Retrieve(int id)
+        {
+            return _ingredientRepository.Retrieve(id);
+        }
+
+        public List<Ingredient> RetrieveAll()
+        {
+            return _ingredientRepository.RetrieveAll();
+        }
+
+        public void Update(Ingredient ingredient)
+        {
+           _ingredientRepository.Update(ingredient);
+        }
+    }
+}
