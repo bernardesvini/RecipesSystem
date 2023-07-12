b250649a2db0b34c731eac579edc90995ef320e5
bernardesvini <vberalves@gmail.com>
2023-07-12 20:05:18 +0100

    IngredientRepository

---
+++ RecipesSystem/Repository/IIngredientRepository.cs
@@ -0,0 +1,15 @@
+﻿using RecipesSystem.Models;
+using System.Collections.Generic;
+
+namespace RecipesSystem.Repository
+{
+    public interface IIngredientRepository
+    {
+        List<Ingredient> RetrieveAll();
+        Ingredient Retrieve(int id);
+        void Create(Ingredient ingredient);
+        void Update(Ingredient ingredient);
+        void Delete(int id);
+
+    }
+}
